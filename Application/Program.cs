671b14109b88fd3afdd9de1c5bb2ae3836ac3c1a
Marco Kreeft <marco.kreeft@gmail.com>
2025-04-27 17:01:52 +0200

    Converted to https for camera

--- Application/Program.cs
+++ Application/Program.cs
@@ -35,7 +35,11 @@ builder.Services.Configure<JsonOptions>(options =>
 });
 
 var app = builder.Build();
-    
+
+// Configure Kestrel to listen on HTTPS
+app.UseHttpsRedirection();  // Optional: this forces HTTP to redirect to HTTPS
+app.UseHsts(); // Enable HTTP Strict Transport Security
+
 // Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
