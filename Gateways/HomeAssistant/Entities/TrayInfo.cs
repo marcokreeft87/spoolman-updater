bc6f643109b27837aa83d951335cc5ceb71e7da0
Marco Kreeft <marco.kreeft@gmail.com>
2025-05-23 10:50:49 +0200

    -

--- Gateways/HomeAssistant/Entities/TrayInfo.cs
+++ Gateways/HomeAssistant/Entities/TrayInfo.cs
@@ -8,20 +8,3 @@ public class TrayInfo
     public string TagUid { get; set; } = string.Empty;
     public string Type { get; set; } = string.Empty;
 }
-
-
-public class BambuServiceRequest
-{
-    [JsonPropertyName("entity_id")]
-    public string[] EntityId { get; set; }
-    [JsonPropertyName("tray_info_idx")]
-    public string TrayInfoIdx { get; set; }
-    [JsonPropertyName("tray_color")]
-    public string TrayColor { get; set; }
-    [JsonPropertyName("tray_type")]
-    public string TrayType { get; set; }
-    [JsonPropertyName("nozzle_temp_min")]
-    public int NozzleTempMin { get; set; }
-    [JsonPropertyName("nozzle_temp_max")]
-    public int NozzleTempMax { get; set; }
-}
